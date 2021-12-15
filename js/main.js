74e43f215cc02cf1d73d4d080bb03c793c6d2f4e
asiedusolo <solomon.asiedu@amalitech.org>
2021-12-15 08:23:28 -0500

    adding fadout text and show frowny text side effects

--- js/main.js
+++ js/main.js
@@ -2,4 +2,19 @@ $(document).ready(() =>{
     $('.hint-box').on('click', () => {
       $('.hint').slideToggle(600);
     })
+
+    $('.wrong-answer-one').on('click', () => {
+        $('.wrong-text-one').fadeOut('slow')
+        $('.frown').show()
+      })
+    
+      $('.wrong-answer-two').on('click', () => {
+        $('.wrong-text-two').fadeOut('slow')
+        $('.frown').show()
+      })
+    
+      $('.wrong-answer-three').on('click', () => {
+        $('.wrong-text-three').fadeOut('slow')
+        $('.frown').show()
+      })
   });
